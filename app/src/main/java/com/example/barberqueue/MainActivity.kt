a305d978af3c2ac05f61c1cdd2a3cd346f59504c
adamasyari <adamasyari05@gmail.com>
2024-06-28 21:53:05 +0800

    update

--- app/src/main/java/com/example/barberqueue/MainActivity.kt
+++ app/src/main/java/com/example/barberqueue/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.barberqueue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.barberqueue.R.layout.activity_main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
     }
 }
---
+++ app/src/main/java/com/example/barberqueue/editProfile.kt
@@ -0,0 +1,11 @@
+package com.example.barberqueue
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class editProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_profile)
+    }
+}
