3dc34f0aea17244d2e12c79f257584197cf9ea75
adamasyari <adamasyari05@gmail.com>
2024-06-29 15:13:20 +0800

    update

--- app/src/main/java/com/example/barberqueue/MainActivity.kt
+++ app/src/main/java/com/example/barberqueue/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.barberqueue
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.barberqueue.R.layout.activity_main
+import android.widget.Button
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var isLayout1Displayed = true
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Button switchButton = new Button(this);
+//        switchButton.setText("Switch Layout");
+//
+//        switchButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (isLayout1Displayed) {
+//                    setContentView(R.layout.layout2);
+//                } else {
+//                    setContentView(R.layout.layout1);
+//                }
+//                isLayout1Displayed = !isLayout1Displayed;
+//            }
+//        });
+//
+//        // Menambahkan tombol ke layout
+//        LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
+//        layout1.addView(switchButton);
     }
+
 }
--- app/src/main/java/com/example/barberqueue/editProfile.kt
+++ app/src/main/java/com/example/barberqueue/editProfile.kt
@@ -1,7 +1,23 @@
 package com.example.barberqueue
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.barberqueue.R.layout.edit_profile
+
 
 class editProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
